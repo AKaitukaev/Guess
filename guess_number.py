f30ae9a8d97cdd26702fb3139504a1f5b42a1742
Alexey Kaitukaev <xronix70@gmail.com>
2025-03-09 15:35:13 +0400

    Change if to elif

--- guess_number.py
+++ guess_number.py
@@ -6,8 +6,8 @@ while True:
     operator_value = int(input('Введите ваше число '))
     if computer_value > operator_value:
         print('Ваше число меньше того, что загадано')
-    if computer_value < operator_value:
+    elif computer_value < operator_value:
         print('Ваше число больше того, что загадано')
-    if computer_value == operator_value:
+    elif computer_value == operator_value:
         print('Отличная интуиция! Вы угадали число :)')
         break
